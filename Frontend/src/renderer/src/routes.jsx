91c4acf4dc6417e5dd93f5124c55c8e1bc7ab811
“AlyssonAntonietti” <alisonantonietti@gmail.com>
2024-06-03 21:08:51 -0300

    Substituido BrowserRouter por HashRouter no arquivo de rotas

--- Frontend/src/renderer/src/routes.jsx
+++ Frontend/src/renderer/src/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import React from 'react';
 
 import HomePage from "./pages/homepage/index";
@@ -7,14 +7,14 @@ import AnexaCurriculo from "./pages/curriculo/anexar/index";
 import Concluido from "./pages/concluido/index";
 
 const AppRoutes = () => (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} ></Route>
                 <Route path="/curriculo/cadastro" element={<CriaCurriculo />}> </Route>
                 <Route path="/curriculo/anexo" element={<AnexaCurriculo />}> </Route>
                 <Route path="/curriculo/concluido" element={<Concluido />}> </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
 )
 
 export default AppRoutes;
